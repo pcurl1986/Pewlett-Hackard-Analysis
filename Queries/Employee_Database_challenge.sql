b1c5e95a5a3aee06ce4f2d71524b3f65b06355fe
pcurl1986 <pcurl1986@gmail.com>
2022-05-03 21:01:28 -0700

    BS

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,39 @@
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+
+INTO retirees_titles
+FROM employees AS e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+SELECT * FROM retirees_titles;
+
+--employee challenge starter code 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title,
+rt.to_date
+INTO unique_titles
+FROM retirees_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+drop table unique_titles cascade;
+
+select * from unique_titles;
+
+--count retirees by title
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+select * from retiring_titles;
