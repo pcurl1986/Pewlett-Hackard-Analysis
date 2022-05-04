6e2a035f061a743a7f648343ccde9105e7d3a880
pcurl1986 <pcurl1986@gmail.com>
2022-05-03 21:09:02 -0700

    message

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -37,3 +37,26 @@ FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 select * from retiring_titles;
+
+--mentorship eligibility table
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+--DISTINCT ON(emp_no)
+INTO mentorship_table
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS t
+				ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+drop table mentorship_table cascade;
+
+select * from mentorship_table;
